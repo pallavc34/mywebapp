fa68c1d0799cd84bbc6134b31ba316cc661c5dae
pallavc34 <pallavc34@gmail.com>
2020-12-05 11:23:56 +0530

    Work sample added

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
@@ -23,6 +24,33 @@ class _HomeScreenState extends State<HomeScreen>
   IconData myThemeIcon = Icons.wb_sunny;
   Color myThemeIconColor = Colors.yellow;
   ThemeData currentTheme = MyConstants().getCurrentTheme(Icons.wb_sunny);
+  AnimationController _animationController;
+  Animation _animation;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(seconds: 2));
+    _animationController.addListener(() {
+      setState(() {});
+    });
+    _animation = CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeInOutBack,
+    );
+    _animationController.forward();
+    _animationController.addStatusListener((status) {
+      setState(() {
+        if (status == AnimationStatus.completed) {
+          _animationController.reverse();
+        } else if (status == AnimationStatus.dismissed) {
+          _animationController.forward();
+        }
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -92,7 +120,7 @@ class _HomeScreenState extends State<HomeScreen>
                             ),
                           ),
                           Text(
-                            "Heyyyy! I am Pallav",
+                            "Hey! I am Pallav",
                             style: GoogleFonts.aBeeZee(
                               fontSize:
                                   isLargeScreen(context) == true ? 50 : 27,
@@ -125,8 +153,8 @@ class _HomeScreenState extends State<HomeScreen>
                             text: [
                               "I am App Developer",
                               "I specialize in Android",
-                              "Engineer by Passion",
-                              "Programmer by profession",
+                              "Engineer by Profession",
+                              "Programmer by passion",
                             ],
                             speed: Duration(milliseconds: 250),
                             textStyle: GoogleFonts.aBeeZee(
@@ -180,13 +208,7 @@ class _HomeScreenState extends State<HomeScreen>
                     SizedBox(
                       height: isSmallScreen(context) == true ? 10.0 : 0.0,
                     ),
-                    Text(
-                      "Services",
-                      style: GoogleFonts.averageSans(
-                          fontSize: isLargeScreen(context) == true ? 50 : 30,
-                          fontWeight: FontWeight.w400,
-                          color: currentTheme.focusColor),
-                    ),
+                    BoldTitleTextWidget(currentTheme: currentTheme,text: "Services",),
                     ResponsiveWidget(
                       largeScreen: Padding(
                         padding: const EdgeInsets.all(10.0),
@@ -197,7 +219,7 @@ class _HomeScreenState extends State<HomeScreen>
                               CardWidget(
                                 title: "Android Developer(Native)",
                                 subtitle:
-                                    "Developing Android apps has always been\na passion. Can develop apps on Native Android\n"
+                                    "Developing Android apps has always been\na passion. I develop apps in Native Android\n"
                                     "as well as Google's Flutter UI framework\n which is popular for beautiful designs.",
                                 iconData: Icons.android,
                                 iconColor: Colors.green,
@@ -206,8 +228,8 @@ class _HomeScreenState extends State<HomeScreen>
                               CardWidget(
                                 title: "iOS Developer(Flutter)",
                                 subtitle:
-                                    "Developing Android apps has always been\na passion. Can develop apps on Native Android\n"
-                                    "as well as Google's Flutter UI framework\n which is popular for beautiful designs.",
+                                    "Cross platform apps that look and feel\nnative. I develop iOS apps in Flutter framework\n"
+                                    "which is popular for beautiful designs\nwith skia, its own graphic engine.",
                                 iconData: Icons.code_rounded,
                                 iconColor: Colors.grey,
                                 themeData: myThemeIcon,
@@ -215,8 +237,8 @@ class _HomeScreenState extends State<HomeScreen>
                               CardWidget(
                                 title: "Web Developer(Flutter)",
                                 subtitle:
-                                    "Developing Android apps has always been\na passion. Can develop apps on Native Android\n"
-                                    "as well as Google's Flutter UI framework\n which is popular for beautiful designs.",
+                                    "Web development made easy with flutter\nEven web apps are now easy to build with\n"
+                                    "flutter and its strong cross platform\n compatibilities.",
                                 iconData: Icons.web,
                                 iconColor: Colors.grey,
                                 themeData: myThemeIcon,
@@ -241,8 +263,8 @@ class _HomeScreenState extends State<HomeScreen>
                             CardWidget(
                               title: "iOS Developer(Flutter)",
                               subtitle:
-                                  "Developing Android apps has always been\na passion. Can develop apps on Native Android\n"
-                                  "as well as Google's Flutter UI framework\nwhich is popular for beautiful designs.",
+                                  "Cross platform apps that look and feel\nnative. I develop iOS apps in Flutter framework\n"
+                                  "which is popular for beautiful designs\nwith skia, its own graphic engine.",
                               iconData: Icons.code_rounded,
                               iconColor: Colors.grey,
                               themeData: myThemeIcon,
@@ -250,8 +272,8 @@ class _HomeScreenState extends State<HomeScreen>
                             CardWidget(
                               title: "Web Developer(Flutter)",
                               subtitle:
-                                  "Developing Android apps has always been\na passion. Can develop apps on Native Android\n"
-                                  "as well as Google's Flutter UI framework\nwhich is popular for beautiful designs.",
+                                  "Web development made easy with flutter\nEven web apps are now easy to build with\n"
+                                  "flutter and its strong cross platform\ncompatibilities.",
                               iconData: Icons.web,
                               iconColor: Colors.grey,
                               themeData: myThemeIcon,
@@ -264,6 +286,131 @@ class _HomeScreenState extends State<HomeScreen>
                 ),
               ),
               //TODO MyWork
+              Center(
+                child: BoldTitleTextWidget(
+                  currentTheme: currentTheme,
+                  text: "My Work",
+                ),
+              ),
+              ResponsiveWidget(
+                largeScreen: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text(
+                            "Personal Diary",
+                            style: GoogleFonts.comfortaa(
+                                fontSize: 40.0,
+                                fontWeight: FontWeight.w400,
+                                color: currentTheme.focusColor),
+                            textAlign: TextAlign.start,
+                          ),
+                          Text(
+                              "A diary which saves all your secrets, keeps track \nof all your moods and looks better than the most",
+                              style: GoogleFonts.comfortaa(
+                                  color: currentTheme.focusColor,
+                                  fontSize: 30.0,
+                                  fontWeight: FontWeight.w400),
+                              textAlign: TextAlign.start)
+                        ],
+                      ),
+                      Transform.rotate(
+                        angle: 0.3,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(25.0),
+                          child: Container(
+                              height: size.height * 0.75,
+                              child: Image.asset("images/diary.jpeg")),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                smallScreen: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Card(
+                    elevation: 15.0,
+                    color: currentTheme.backgroundColor,
+                    shadowColor: myThemeIcon == Icons.wb_sunny ? Colors.black.withOpacity(0.5) : Colors.white.withOpacity(0.5),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25.0),
+                      side: BorderSide(
+                        color: myThemeIcon == Icons.wb_sunny ? Colors.black.withOpacity(0.7) : Colors.white.withOpacity(0.7),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Personal Diary",
+                            style: GoogleFonts.aBeeZee(
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.w400,
+                                color: currentTheme.focusColor),
+                            textAlign: TextAlign.start,
+                          ),
+                          Text(
+                              "A diary which saves all your secrets, keeps track \nof all your moods and looks better than the most",
+                              style: GoogleFonts.aBeeZee(
+                                  color: currentTheme.focusColor,
+                                  fontSize: 15.0,
+                                  fontWeight: FontWeight.w400),
+                              textAlign: TextAlign.start),
+                          CarouselSlider.builder(
+                            itemCount: 3,
+                            options: CarouselOptions(
+                              enableInfiniteScroll: true,
+                              autoPlay: true,
+                              enlargeCenterPage: true,
+                            ),
+                            itemBuilder: (context, index){
+                              var string = ['Beautiful UI','Expressive','Customizable'];
+                              var diary = ['diary.jpeg','diary2.jpeg','diary3.jpeg'];
+                              return Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                  color: Color(0xFFd1c4e9),
+                                ),
+                                padding: EdgeInsets.all(10.0),
+                                child: Column(
+                                  children: [
+                                    Text(string[index]),
+                                    SizedBox(height: 5.0,),
+                                    Expanded(
+                                      child: ClipRRect(
+                                        borderRadius: BorderRadius.circular(10.0),
+                                        child: Container(
+                                            height: 250,
+                                            child: Image.asset("images/${diary[index]}")),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              );
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              //Footer
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Text(
+                    "Designed and Developed by Pallav Chaudhari. Copyright @ 2020",
+                style: TextStyle(
+                  color: currentTheme.focusColor
+                ),),
+              )
             ],
           ),
         ),
@@ -291,6 +438,27 @@ class _HomeScreenState extends State<HomeScreen>
         color: myThemeIconColor,
         size: 40.0,
       ),
+    );
+  }
+}
+
+class BoldTitleTextWidget extends StatelessWidget {
+  BoldTitleTextWidget({
+    @required this.currentTheme,
+    @required this.text,
+  });
+
+  final ThemeData currentTheme;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: GoogleFonts.averageSans(
+          fontSize: isLargeScreen(context) == true ? 50 : 30,
+          fontWeight: FontWeight.w400,
+          color: currentTheme.focusColor),
     );
   }
 }
